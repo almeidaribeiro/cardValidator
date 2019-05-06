5c7bae42dabf74e2c514749d87dd604e3959dfce
Boo <almeidaribeiro.b@gmail.com>
2019-05-06 01:13:43 -0300

    arrumando gitignore

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,16 +1,28 @@
-/*eslint-disable no-undef */
+/* eslint-disable no-undef */
 
 let chai = require('chai');
 let expect = chai.expect;
-let cardValidator =require('../lib/index');
+let cardValidator = require('../lib/index');
 
 describe('card lib', () => {
-    describe('cardValidator', () =>{
-      it('card is invalid', () => {
-        expect(cardValidator.cardValidator('4716421870804443')).to.equal(false);
-      });
-      it('card is valid', () => {
-        expect(cardValidator.cardValidator('4929534188660375')).to.equal(true);
-      });
+  describe('cardValidator', () =>{
+    it('card is invalid', () => {
+      expect(cardValidator.cardValidator('4716421870804443')).to.equal(false);
     });
+    it('card is valid', () => {
+      expect(cardValidator.cardValidator('4929534188660375')).to.equal(true);
+    });
+    it('card is invalid', () => {
+      expect(cardValidator.cardValidator(4929534188660375)).to.equal(false);
+    });
+    it('card is invalid', () => {
+      expect(cardValidator.cardValidator('49295341886603')).to.equal(false);
+    });
+    it('card is invalid', () => {
+      expect(cardValidator.cardValidator('ribeiro')).to.equal(false);
+    });
+    it('card is valid', () => {
+      expect(cardValidator.cardValidator('4024007156968108')).to.equal(true);
+    });
+  });
 });
