accf206870f953a9e30c75ba7eecb2fb0443357d
Boo <almeidaribeiro.b@gmail.com>
2019-05-07 00:21:10 -0300

    testes finalizados

--- test/index.spec.js
+++ test/index.spec.js
@@ -9,6 +9,15 @@ describe('card lib', () => {
     it('it should be true', () => {
       expect(cardValidator.cardValidator('4929534188660375')).to.equal(true);
     });
+    it('it should be true', () => {
+      expect(cardValidator.cardValidator('4556 6891 4457 4795')).to.equal(true);
+    });
+    it('it should be true', () => {
+      expect(cardValidator.cardValidator('4532 5622 3572 9702')).to.equal(true);
+    });
+    it('it should be true', () => {
+      expect(cardValidator.cardValidator('4532024166762848')).to.equal(true);
+    });
     it('it should be false', () => {
       expect(cardValidator.cardValidator('4716421870804443')).to.equal(false);
     });
@@ -24,6 +33,11 @@ describe('card lib', () => {
     it('it should be false', () => {
       expect(cardValidator.cardValidator('0000000000000000')).to.equal(false);
     });
-
+    it('it should be false', () => {
+      expect(cardValidator.cardValidator('4929534188660')).to.equal(false);
+    });
+    it('it should be false', () => {
+      expect(cardValidator.cardValidator('123456789123456789')).to.equal(false);
+    });
   });
 });
